9f157d8b64ff247aeb61ae666a8ba95a3010a5ec
strinner213 <lena.scholz213@gmail.com>
2026-09-23 15:51:09 +0200

    Fix faulty main file

--- MSUtils/neper/main_graph.py
+++ MSUtils/neper/main_graph.py
@@ -3,25 +3,27 @@ from pathlib import Path
 from MSUtils.neper.NeperTessellation import NeperTessellation
 from MSUtils.neper.PolycrystalGraph import PolycrystalGrainGraph, PolycrystalFacetEnhancedGraph, PolycrystalVertexEnhancedGraph
 
+_PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
 
 def main():
-    name = "diamond"
-    tess = NeperTessellation(f"/home/scholz/workspace/MSUtils/data/neper/{name}.tess")
+    name = "periodic_voronoi"
+    tess = NeperTessellation(f"{_PROJECT_ROOT}/data/neper/{name}.tess")
 
     graph = PolycrystalGrainGraph(tess, [], [])
     graph.graph_stats()
-    graph.visualize_3d(Path(f"./data/grain_graph_{name}"), node_feature="boundary")
-    graph.write_h5(Path(f"./data/grain_graph_{name}"), node_features=["position", "boundary"], edge_features=[])
+    graph.visualize_3d(Path(f"{_PROJECT_ROOT}/data/{name}_grain_graph"), node_feature="type")
+    graph.write_h5(Path(f"{_PROJECT_ROOT}/data/{name}_grain_graph"), node_features=["position", "type", "boundary"], edge_features=[])
 
     graph = PolycrystalFacetEnhancedGraph(tess, [], [])
     graph.graph_stats()
-    graph.visualize_3d(Path(f"./data/facet_graph_{name}"), node_feature="boundary")
-    graph.write_h5(Path(f"./data/facet_graph_{name}"), node_features=["position", "boundary"], edge_features=[])
+    graph.visualize_3d(Path(f"{_PROJECT_ROOT}/data/{name}_facet_graph"), node_feature="type")
+    graph.write_h5(Path(f"{_PROJECT_ROOT}/data/{name}_facet_graph"), node_features=["position", "type", "boundary"], edge_features=[])
 
     graph = PolycrystalVertexEnhancedGraph(tess, [], [])
     graph.graph_stats()
-    graph.visualize_3d(Path(f"./data/vertex_graph_{name}"), node_feature="boundary")
-    graph.write_h5(Path(f"./data/vertex_graph_{name}"), node_features=["position", "boundary"], edge_features=[])
+    graph.visualize_3d(Path(f"{_PROJECT_ROOT}/data/{name}_vertex_graph"), node_feature="type")
+    graph.write_h5(Path(f"{_PROJECT_ROOT}/data/{name}_vertex_graph"), node_features=["position", "type", "boundary"], edge_features=[])
 
 
 if __name__ == "__main__":
